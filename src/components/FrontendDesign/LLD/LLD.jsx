fcbd2d294fe74bf93837e82bfedab274ee14f22c
Aditya Malvi <adityamalvi@Adityas-MacBook-Pro.local>
2025-11-29 19:41:37 +0530

    lld - config driven ui

--- src/components/FrontendDesign/LLD/LLD.jsx
+++ src/components/FrontendDesign/LLD/LLD.jsx
@@ -1,9 +1,11 @@
 import ComponentDesign from './ComponentDesign'
+import ConfigDrivenUI from './ConfigDrivenUI'
 
 function LLD () {
   return (
     <div>
       <ComponentDesign />
+      <ConfigDrivenUI />
     </div>
   )
 }
